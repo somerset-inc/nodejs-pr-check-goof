536af325d6310bef382a899b1bcc75f4b40bb864
thavelock <103456846+thavelock@users.noreply.github.com>
2025-12-24 10:43:26 -0500

    Add second token variable and log to console

--- app.js
+++ app.js
@@ -87,6 +87,9 @@ if (app.get('env') == 'development') {
 var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
 console.log('token: ' + token);
 
+var token2 = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
+console.log('token: ' + token2);
+
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
